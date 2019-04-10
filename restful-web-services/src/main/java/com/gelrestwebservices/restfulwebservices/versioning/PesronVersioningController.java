0e635b964be8ee21eb831337993a4e45ba435cba
IamGiel <november14giel@gmail.com>
2019-04-10 10:30:46 -0400

    basic authentication

--- restful-web-services/src/main/java/com/gelrestwebservices/restfulwebservices/versioning/PesronVersioningController.java
+++ restful-web-services/src/main/java/com/gelrestwebservices/restfulwebservices/versioning/PesronVersioningController.java
@@ -6,6 +6,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class PesronVersioningController {
 	
+	//different versioning of API
+	
 	@GetMapping("v1/person")
 	public PersonV1 personV1() {
 		return new PersonV1("Bob Charlie");
@@ -13,6 +15,39 @@ public class PesronVersioningController {
 	
 	@GetMapping("v2/person")
 	public PersonV2 personV2() {
+		return new PersonV2(new Name("James", "Corden"));
+	}
+	
+	// request parameters
+	@GetMapping(value="/person/param", params="version=1")
+	public PersonV1 paramV1() {
+		return new PersonV1("Bob Charlie");
+	}
+	
+	@GetMapping(value="/person/param", params="version=2")
+	public PersonV2 paramV2() {
+		return new PersonV2(new Name("James", "Corden"));
+	}
+	
+	// Header parameters
+	@GetMapping(value="/person/header", headers="X-API-VERSION=1")
+	public PersonV1 headerV1() {
+		return new PersonV1("Bob Charlie");
+	}
+	
+	@GetMapping(value="/person/header", headers="X-API-VERSION=2")
+	public PersonV2 headerV2() {
+		return new PersonV2(new Name("James", "Corden"));
+	}
+	
+	// Produces parameters or MIMETYPE versioning
+	@GetMapping(value="/person/produces", produces="application/vnd.company.app-v1+json")
+	public PersonV1 producesV1() {
+		return new PersonV1("Bob Charlie");
+	}
+	
+	@GetMapping(value="/person/produces", produces="application/vnd.company.app-v2+json")
+	public PersonV2 producesV2() {
 		return new PersonV2(new Name("James", "Corden"));
 	}
 }
